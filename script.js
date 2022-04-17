fd9212ce78b1005c31b7d000537310973d37b3d3
Rafaela Oliveira <labdarafaela@gmail.com>
2022-04-17 16:03:47 -0400

    Hamburger Menu Adjustments

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#menu-links');
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
+
+// document.querySelectorAll(".nav-link").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//   })
+// );
+
+
+hamburger.addEventListener('click', () => {
+    console.log(hamburger);
+    console.log(navMenu);
+    navMenu.classList.toggle('active');
+})
